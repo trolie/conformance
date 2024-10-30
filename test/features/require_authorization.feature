5a6a179bd968c2ac96c2b508e0e11c5b914909b6
Christopher Atkins <catkins@misoenergy.org>
2024-10-30 13:07:06 +0000

    test historical forecast snapshot auth

--- test/features/require_authorization.feature
+++ test/features/require_authorization.feature
@@ -1,7 +1,14 @@
 
 Feature: All requests require authorization
+  
+  Background:
+    Given a TROLIE client that has not been authorized
+
 
   Scenario: Get Forecast Limits Snapshot requires authorization
-    Given a TROLIE client that has not been authorized
     When the client requests the current Forecast Limits Snapshot
+    Then the response is Unauthorized
+
+  Scenario: Get Historical Forecast Limits Snapshot requires authorization
+    When the client requests a Historical Forecast Limits Snapshot
     Then the response is Unauthorized
--- test/step_defs/test_require_authorization.py
+++ test/step_defs/test_require_authorization.py
@@ -1,28 +1,42 @@
 
-import os
+from datetime import datetime, timedelta
+import os, time, pytz
 from pytest_bdd import scenario, given, when, then
 import pytest
 import requests
 
-def trolie_url(relative_path):
-    return os.getenv('TROLIE_BASE_URL') + relative_path
+def trolie_request(relative_path, auth_token=None, method='GET'):
+    trolie_url = os.getenv('TROLIE_BASE_URL') + relative_path
+    return requests.request(method=method, url=trolie_url)
+
+def get_period(hours=0):
+    tz_name = os.getenv('TZ', 'UTC')
+    timezone = pytz.timezone(tz_name)
+    return (datetime.now(timezone) + timedelta(hours=hours)).replace(minute=0, second=0, microsecond=0).isoformat()
 
 @pytest.mark.Forecasting
 @scenario('require_authorization.feature', 'Get Forecast Limits Snapshot requires authorization')
-def test_authorization():
+def test_snapshot_authorization():
     pass
 
+@pytest.mark.Forecasting
+@scenario('require_authorization.feature', 'Get Historical Forecast Limits Snapshot requires authorization')
+def test_historical_snapshot_authorization():
+    pass
 
-@given('a TROLIE client that has not been authorized', target_fixture='client')
+@given('a TROLIE client that has not been authorized', target_fixture='auth_token')
 def client_not_authorized():
-    return requests.Request(method='GET', url=trolie_url('/limits/forecast-snapshot'))
+    return None
 
 @when('the client requests the current Forecast Limits Snapshot',
       target_fixture='response')
-def request_forecast_limits_snapshot(client):
-    session = requests.Session()
-    prepared_request = session.prepare_request(client)
-    return session.send(prepared_request)
+def request_forecast_limits_snapshot():
+    return trolie_request('/limits/forecast-snapshot')
+
+@when('the client requests a Historical Forecast Limits Snapshot',
+      target_fixture='response')
+def request_forecast_limits_snapshot():
+    return trolie_request(f"/limits/forecast-snapshot/{get_period(-1)}")
 
 @then('the response is Unauthorized')
 def response_is_unauthorized(response):
